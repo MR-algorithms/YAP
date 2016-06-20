2f6d144c025f15f9a051480e9bedcf0c17f646e0
luoqing <1361224053@qq.com>
2016-06-20 22:12:08 +0800

    Add Grappa

---
+++ BasicRecon/Grappa.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Grappa.h"
+
+
+CGrappa::CGrappa()
+{
+}
+
+
+CGrappa::~CGrappa()
+{
+}
---
+++ BasicRecon/Grappa.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ProcessorImp.h"
+class CGrappa :
+	public CProcessorImp
+{
+public:
+	CGrappa();
+	virtual ~CGrappa();
+};
+
