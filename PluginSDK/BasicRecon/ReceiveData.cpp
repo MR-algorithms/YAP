55d3d7a7aee69773c4d6a643a4d9d34c5ea82e10
LiDongbao <1518425008@qq.com>
2016-09-07 21:29:03 +0800

    add

---
+++ PluginSDK/BasicRecon/ReceiveData.cpp
@@ -0,0 +1,82 @@
+#include "stdafx.h"
+#include "ReceiveData.h"
+#include "Interface\IData.h"
+#include "Interface/Client/DataHelper.h"
+
+using namespace Yap;
+using namespace std;
+namespace Yap
+{
+
+	ReceiveData::ReceiveData() :
+		ProcessorImpl(L"ReceiveData")
+	{
+	}
+
+	ReceiveData::~ReceiveData()
+	{
+	}
+
+	bool ReceiveData::OnInit()
+	{
+		AddInput(L"Input", 1, 
+			DataTypeComplexDouble | 
+			DataTypeComplexFloat | 
+			DataTypeFloat | 
+			DataTypeDouble | 
+			DataTypeInt | 
+			DataTypeShort | 
+			DataTypeUnsignedInt | 
+			DataTypeUnsignedShort);
+		AddOutput(L"Output", YAP_ANY_DIMENSION, 
+			DataTypeComplexDouble | 
+			DataTypeComplexFloat | 
+			DataTypeFloat |
+			DataTypeDouble |
+			DataTypeInt |
+			DataTypeShort |
+			DataTypeUnsignedInt |
+			DataTypeUnsignedShort);
+		AddProperty(PropertyInt, L"DataCount", L"The count of data flow times.");
+		AddProperty(PropertyStruct, L"DataDimension", L"The dimension size of data and every dimension size as an Array.");
+	}
+
+	IProcessor * ReceiveData::Clone()
+	{
+		return new(nothrow) ReceiveData(*this);
+	}
+
+	bool ReceiveData::Input(const wchar_t * name, IData * data)
+	{
+		if (name != L"Input")
+			return false;
+		if (data == nullptr)
+			return false;
+		CDataHelper data_reverse = CDataHelper(data);
+		auto data_type = data_reverse.GetDataType();
+		switch (data_type)
+		{
+			case DataTypeComplexDouble:
+				
+				break;
+			case DataTypeComplexFloat:
+				break;
+			case DataTypeDouble:
+				break;
+			case DataTypeFloat:
+				break;
+			case DataTypeInt:
+				break;
+			case DataTypeShort:
+				break;
+			case DataTypeUnsignedInt:
+				break;
+			case DataTypeUnsignedShort:
+				break;
+			default:
+				
+		}
+
+	}
+
+}
