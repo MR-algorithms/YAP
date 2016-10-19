87614586a66e27663385a6a1b68a8b0ec75a4b0c
LiDongbao <1518425008@qq.com>
2016-10-20 00:45:27 +0800

    Do be rubust to GLCM.
    添加不同方向的GLCM提取；
    添加不同步幅的GLCM提取。

--- PluginSDK/BasicRecon/ExtractGLCM.cpp
+++ PluginSDK/BasicRecon/ExtractGLCM.cpp
@@ -3,6 +3,9 @@
 #include "Interface/Client/DataHelper.h"
 #include <math.h>
 
+using namespace std;
+using namespace Yap;
+using namespace Yap::Direction;
 
 template<typename T>
 void Convert(Yap::DataHelper help, unsigned int * out_data, unsigned int size)
@@ -26,7 +29,10 @@ void Convert(Yap::DataHelper help, unsigned int * out_data, unsigned int size)
 	}
 }
 Yap::ExtractGLCM::ExtractGLCM():
-	ProcessorImpl(L"ExtractGLCM")
+	ProcessorImpl(L"ExtractGLCM"),
+	_glcm_size(256),
+	_direction(0),
+	_step_size(1)
 {
 }
 
@@ -49,19 +55,25 @@ bool Yap::ExtractGLCM::Input(const wchar_t * name, IData * data)
 	
 	if (data_helper.GetActualDimensionCount() != 2)
 		return false;
-	unsigned int size = GetInt(L"GLCMSize");
-	assert(size >= 8 && size <= 256);
+	unsigned int glcm_size = GetInt(L"GLCMSize");
+	unsigned int direction = GetInt(L"Direction");
+	unsigned int step_size = GetInt(L"StepSize");
+	if (direction > 12 || direction < 0 || step_size >data_helper.GetDataSize() || step_size < 0 || glcm_size < 8 || glcm_size > 256)
+		return false;
+	SetGLCMSize(glcm_size);
+	SetDirection(direction);
+	SetStepSize(step_size);
 
 	auto normalize_data = new unsigned int[data_helper.GetDataSize()];
 
-	Normalization(data, normalize_data, size);
+	Normalization(data, normalize_data);
 	
-	auto glcm = new unsigned int[(size_t)size * size];
+	auto glcm = new unsigned int[(size_t)glcm_size * glcm_size];
 
-	GLCM(normalize_data, glcm, data_helper.GetWidth(), data_helper.GetHeight(), size);
+	GLCM(normalize_data, glcm, data_helper.GetWidth(), data_helper.GetHeight());
 
 	Dimensions dimensions;
-	dimensions(DimensionReadout, 0U, size)(DimensionPhaseEncoding, 0U, size);
+	dimensions(DimensionReadout, 0U, glcm_size)(DimensionPhaseEncoding, 0U, glcm_size);
 	auto glcm_shared = Yap::YapShared(new UnsignedIntData(glcm, dimensions, nullptr, true));
 
 	Feed(L"Output", glcm_shared.get());
@@ -78,35 +90,182 @@ Yap::IProcessor * Yap::ExtractGLCM::Clone()
 bool Yap::ExtractGLCM::OnInit()
 {
 	AddProperty(PropertyInt, L"GLCMSize", L"The GLCM matrix size(gray level size).");
+	AddProperty(PropertyInt, L"Direction", L"The GLCM matrix direction.");
+	AddProperty(PropertyInt, L"StepSize", L"The GLCM matrix from derived image step,eg: StepSize = 1, then get image pixels with distance 1.");
 	SetInt(L"GLCMSize", 256);
+	SetInt(L"Direction", Yap::Direction::DirectionAll);
+	SetInt(L"StepSize", 1);
 	//All Type data to Int (0~n) n:8~255
 	AddInput(L"Input", 2 , DataTypeAll);
 	AddOutput(L"Output", 2 , DataTypeUnsignedInt);
 	return true;
 }
 
-void Yap::ExtractGLCM::GLCM(unsigned int * input_data, unsigned int * output_data, unsigned int input_width, unsigned int input_height, unsigned int out_size)
+void Yap::ExtractGLCM::GLCM(unsigned int * input_data, 
+	unsigned int * output_data, 
+	unsigned int input_width, 
+	unsigned int input_height)
 {
-
+	auto out_size = GetGLCMSize();
 	//init glcm
 	for (size_t i = 0; i < out_size * out_size; ++i)
 	{
 		output_data[i] = 0;
 	}
-	//仅仅实现横向GLCM，row不变，column + 1
-	for (size_t i = 0; i < input_width; ++i)
+
+	switch (GetDirection())
 	{
-		for (size_t j = 0; j < input_height - 1; ++j)
+		/*
+		* 灰度共生矩阵实现不同方向，不同步幅；
+		*	r\c	|	-n		0		n
+		*----------------------------------
+		*	-n	|	l-u-n	u-n		r-u-n
+		*	 0	|	l-n		0		r-n
+		*	 n	|	l-d-n	d-n		r-d-n
+		* 注：l-左，u-上，r-右，d-下，n-步幅为n
+		*/
+		case DirectionRight:
 		{
-			auto row = input_data[i * input_width + j];
-			auto column = input_data[i * input_width + j + 1];
-			++output_data[row * (size_t)out_size + column];
+			int r = 0;
+			int c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			//for (unsigned int i = (r < 0 ? 1 : 0); i < input_width - (r > 0 ? -1 : 0); ++i)
+			//	{
+			//		for (unsigned int j = (c < 0? 1:0); j < input_height + (c > 0? -1:0); ++j)
+			//		{
+			//			auto row = input_data[(i + r) * input_width + j];
+			//			auto column = input_data[i * input_width + j + c];
+			//			++output_data[row * out_size + column];
+			//		}
+			//	}
+			break;
 		}
+		case DirectionDown:
+		{
+			int r = 1;
+			int c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionLeft:
+		{
+			int r = 0;
+			int c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionUp:
+		{
+			int r = -1;
+			int c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionRightDown:
+		{
+			int r = 1;
+			int c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionLeftDown:
+		{
+			int r = 1;
+			int c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionLeftUp:
+		{
+			int r = -1;
+			int c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionRightUp:
+		{
+			int r = -1;
+			int c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionRightAndDown:
+		{
+			int r = 0;
+			int c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = 1;
+			c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionLeftAndDown:
+		{
+			int r = 0;
+			int c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = 1;
+			c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionLeftAndUp:
+		{
+			int r = 0;
+			int c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = -1;
+			c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionRightAndUp:
+		{
+			int r = 0;
+			int c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = -1;
+			c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		case DirectionAll:
+		{
+			//实现上、下、左、右、右下、左下、左上、右上，四个方向。
+			int r = 0;
+			int c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = 1;
+			c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = 0;
+			c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = -1;
+			c = 0;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = 1;
+			c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = 1;
+			c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = -1;
+			c = -1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			r = -1;
+			c = 1;
+			AddDirection(input_data, output_data, input_width, input_height, out_size, r, c);
+			break;
+		}
+		default:
+			return ;
 	}
 }
 
-void Yap::ExtractGLCM::Normalization(IData * data, unsigned int * out, unsigned int size)
+void Yap::ExtractGLCM::Normalization(IData * data, unsigned int * out)
 {
+	auto size = GetGLCMSize();
 	assert(size <= 256);
 	DataHelper help(data);
 
@@ -183,4 +342,69 @@ void Yap::ExtractGLCM::Normalization(IData * data, unsigned int * out, unsigned 
 			return;
 	}
 
+}
+
+void Yap::ExtractGLCM::SetGLCMSize(unsigned int glcm_size)
+{
+	_glcm_size = glcm_size;
+}
+
+unsigned int Yap::ExtractGLCM::GetGLCMSize()
+{
+	return _glcm_size;
+}
+
+void Yap::ExtractGLCM::SetDirection(unsigned int direction)
+{
+	_direction = direction;
+}
+
+unsigned int Yap::ExtractGLCM::GetDirection()
+{
+	return _direction;
+}
+
+void Yap::ExtractGLCM::SetStepSize(unsigned int step_size)
+{
+	_step_size = step_size;
+}
+
+unsigned int Yap::ExtractGLCM::GetStepSize()
+{
+	return _step_size;
+}
+
+/*
+*在求GLCM的时候需要判断不同方向对原图像矩阵横纵边界的影响：
+*	step_r		i		width
+*--------------------------------
+*		 n		0			-n
+*		 0		0			 0
+*		-n		n			 0
+*so: unsigned int step_r = (step_r < 0 ? n : 0); and row = ***[(i + step_r)***]***
+*
+*	step_c		j		height
+*-------------------------------
+*		 n		0			-n
+*		 0		0			 0
+*		-n		n			 0
+*so: unsigned int step_c = (step_c < 0 ? n : 0); and column = ***[*** + j + step_c];
+*/
+void Yap::ExtractGLCM::AddDirection(unsigned int * input_data, 
+	unsigned int * output_data, 
+	unsigned int input_width, 
+	unsigned int input_height, 
+	unsigned int out_size, 
+	int step_row, 
+	int step_column)
+{
+	for (unsigned int i = (step_row < 0 ? 1 : 0); i < input_width - (step_row > 0 ? -1 : 0); ++i)
+	{
+		for (unsigned int j = (step_column < 0 ? 1 : 0); j < input_height + (step_column > 0 ? -1 : 0); ++j)
+		{
+			auto row = input_data[(i + step_row) * input_width + j];
+			auto column = input_data[i * input_width + j + step_column];
+			++output_data[row * out_size + column];
+		}
+	}
 }
--- PluginSDK/BasicRecon/ExtractGLCM.h
+++ PluginSDK/BasicRecon/ExtractGLCM.h
@@ -3,6 +3,23 @@
 
 namespace Yap 
 {
+	namespace Direction
+	{
+		const unsigned int DirectionRight			= 0;
+		const unsigned int DirectionDown			= 1;
+		const unsigned int DirectionLeft			= 2;
+		const unsigned int DirectionUp				= 3;
+		const unsigned int DirectionRightDown		= 4;
+		const unsigned int DirectionLeftDown		= 5;
+		const unsigned int DirectionLeftUp			= 6;
+		const unsigned int DirectionRightUp			= 7;
+		const unsigned int DirectionRightAndDown	= 8;
+		const unsigned int DirectionLeftAndDown		= 9;
+		const unsigned int DirectionLeftAndUp		= 10;
+		const unsigned int DirectionRightAndUp		= 11;
+		const unsigned int DirectionAll				= 12;
+	};
+
 	class ExtractGLCM :
 		public ProcessorImpl
 	{
@@ -15,10 +32,35 @@ namespace Yap
 		virtual IProcessor * Clone() override;
 
 		virtual bool OnInit() override;
-	private:
-		void GLCM(unsigned int * input_data,  unsigned int * output_data, unsigned int input_width, unsigned int input_height, unsigned int out_size);
-		void Normalization(IData * data, unsigned int * out, unsigned int size);	//the GLCM size or gray level number.
-	};
+		
+		void SetDirection(unsigned int direction);
+		unsigned int GetDirection();
 
+		void SetStepSize(unsigned int step_size);
+		unsigned int GetStepSize();
+
+		void SetGLCMSize(unsigned int glcm_size);
+		unsigned int GetGLCMSize();
+
+	private:
+		void GLCM(unsigned int * input_data,  
+			unsigned int * output_data, 
+			unsigned int input_width, 
+			unsigned int input_height);
+		void Normalization(IData * data, unsigned int * out);	//the GLCM size or gray level number.
+
+		void AddDirection(unsigned int * input_data,
+			unsigned int * output_data,
+			unsigned int input_width,
+			unsigned int input_height,
+			unsigned int out_size,
+			int step_row,
+			int step_column);
+
+	private:
+		unsigned int _glcm_size;
+		unsigned int _direction;
+		unsigned int _step_size;
+	};
 }
 
