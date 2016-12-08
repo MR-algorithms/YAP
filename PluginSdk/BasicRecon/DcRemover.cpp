f4e0190592ab4d2e826f31993776123792702d03
Li_WenJing <ywdj90@126.com>
2016-12-08 16:20:04 +0800

    Modify 'RemoveDC' to 'DcRemover

--- PluginSdk/BasicRecon/DcRemover.cpp
+++ PluginSdk/BasicRecon/DcRemover.cpp
@@ -57,7 +57,7 @@ void RemoveDC(T * input_data,
 }
 
 DcRemover::DcRemover() :
-	ProcessorImpl(L"RemoveDC")
+	ProcessorImpl(L"DcRemover")
 {
 	AddInput(L"Input", 2, DataTypeComplexDouble | DataTypeComplexFloat);
 	
