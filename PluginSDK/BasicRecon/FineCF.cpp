bc300b8b714424bd7bdc51c4a2b13848689f4ab8
zcx <angcx@126.com>
2017-05-23 20:07:53 +0800

    添加FineCF处理器。

---
+++ PluginSDK/BasicRecon/FineCF.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "FineCF.h"
+#include "Client\DataHelper.h"
+#include <algorithm>
+#include <vector>
+#include <cassert>
+using namespace std;
+using namespace Yap;
+FineCF::FineCF()
+	: ProcessorImpl(L"FineCF")
+{
+	AddInput(L"Input", 1, DataTypeDouble | DataTypeFloat);
+	AddProperty<double>(L"O1", 0, L"中心频率");
+	AddProperty<double>(L"SW", 0, L"谱宽");
+	AddProperty<bool>(L"ReverseAxis", false, L"负轴");
+	AddProperty<double>(L"CF", 0, L"最高峰频率");
+}
+
+
+FineCF::~FineCF()
+{
+}
+
+bool FineCF::Input(const wchar_t * port, IData * data)
+{
+	if (data == nullptr && _wcsicmp(port, L"Input") != 0)
+		return false;
+
+	if (data->GetDataType() != DataTypeDouble && data->GetDataType() != DataTypeFloat)
+		return false;
+
+	DataHelper input_data(data);
+	if (input_data.GetActualDimensionCount() != 1)
+		return false;
+
+	unsigned int width = input_data.GetWidth();
+	auto o1 = GetProperty<double>(L"O1");
+	auto sw = GetProperty<double>(L"SW");
+	int max_val_pos = 0;
+	if (data->GetDataType() == DataTypeDouble)
+	{
+		std::vector<double> temp_data(width, 0);
+		std::memcpy(temp_data.data(), GetDataArray<double>(data), width * sizeof(double));
+		max_val_pos = std::max_element(temp_data.begin(), temp_data.end()) - temp_data.begin();
+	}
+	else
+	{
+		assert(data->GetDataType() == DataTypeFloat);
+		std::vector<float> temp_data(width, 0);
+		std::memcpy(temp_data.data(), GetDataArray<float>(data), width * sizeof(float));
+		max_val_pos = std::max_element(temp_data.begin(), temp_data.end()) - temp_data.begin();
+	}
+	double offset = (max_val_pos - width / 2) * sw / width;
+	SetProperty<double>(L"CF", GetProperty<bool>(L"ReverseAxis") ? o1 - offset : o1 + offset);
+	return true;
+}
---
+++ PluginSDK/BasicRecon/FineCF.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Implement\ProcessorImpl.h"
+#include "Interface\smartptr.h"
+
+namespace Yap
+{
+	class FineCF :
+		public ProcessorImpl
+	{
+		IMPLEMENT_SHARED(FineCF)
+	public:
+		FineCF();
+
+	protected:
+		~FineCF();
+
+		virtual bool Input(const wchar_t * port, IData * data) override;
+
+		// property input O1, SW
+		// property output CF;
+	private:
+	};
+
+}
+
